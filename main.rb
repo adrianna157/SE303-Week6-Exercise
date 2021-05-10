b4256396fc009ada4327762f054576366257fec3
Adrianna Guevarra <guevarra@oregonstate.edu>
2021-05-09 20:56:24 -0700

    delete standalone draw_button function

--- main.rb
+++ main.rb
@@ -1,15 +1,3 @@
-
-
-def draw_button(label_text, x, y, foreground_color, is_dark_mode)
-  if is_dark_mode
-    # darken foreground color for dark mode
-    paint(label_text, x, y, foreground_color - 10, '#111111')
-  else
-    # lighten foreground color for non-dark mode
-    paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
-  end
-end
-
 button = Button.new("dark", 2, 5, 20, '#111111')
 button.draw_button
 
