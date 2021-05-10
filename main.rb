37ac49f05f19c9f6190524a23c647e4967fb0d26
Adrianna Guevarra <guevarra@oregonstate.edu>
2021-05-09 20:49:52 -0700

    initialize member variable for dark_mode or light_mode

--- main.rb
+++ main.rb
@@ -12,6 +12,10 @@ end
 
 class Button
     attr_reader :label_text, :x, :y, :foreground_color, :mode 
+    
+    @@light_mode == '#E0E0E0'
+    @@dark_mode == '#111111'
+    
     def initialize (label_text, x, y, foreground_color, mode)
         @label_text = label_text
         @x = x
@@ -20,13 +24,13 @@ class Button
         @mode = mode
     end
 
-    def draw_button(label_text, x, y, foreground_color, is_dark_mode)
-        if is_dark_mode
+    def draw_button
+        if mode == '#111111'
             # darken foreground color for dark mode
-            paint(label_text, x, y, foreground_color - 10, '#111111')
+            paint(label_text, x, y, foreground_color - 10, mode)
         else
             # lighten foreground color for non-dark mode
-            paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+            paint(label_text, x, y, foreground_color + 10, mode)
         end
         end
 end
