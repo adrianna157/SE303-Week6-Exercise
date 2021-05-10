6e39565aacaf0ed951e5c92ad9b39ff6f2c43bd9
Adrianna Guevarra <guevarra@oregonstate.edu>
2021-05-09 21:01:35 -0700

    switch order of class definition and object instantiation

--- main.rb
+++ main.rb
@@ -1,11 +1,9 @@
-button = Button.new("dark", 2, 5, 20, '#111111')
-button.draw_button
 
 class Button
     attr_reader :label_text, :x, :y, :foreground_color, :mode 
     
-    @@light_mode == '#E0E0E0'
-    @@dark_mode == '#111111'
+    @@light_mode = '#E0E0E0'
+    @@dark_mode = '#111111'
     
     def initialize (label_text, x, y, foreground_color, mode)
         @label_text = label_text
@@ -19,11 +17,17 @@ class Button
         if mode == @@dark_mode
             mode = @@dark_mode
             # darken foreground color for dark mode
-            paint(label_text, x, y, foreground_color - 10, mode)
+            puts(label_text, x, y, foreground_color - 10, mode)
         else
             mode == @light_mode
             # lighten foreground color for non-dark mode
-            paint(label_text, x, y, foreground_color + 10, mode)
+            puts(label_text, x, y, foreground_color + 10, mode)
         end
         end
 end
+
+
+
+
+button = Button.new("dark", 2, 5, 20, '#111111')
+button.draw_button
